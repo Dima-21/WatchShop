43db0ab60c932e5c9f7aa91c903775583c2730b0
Dima-21 <zhmurchuk21@gmail.com>
2019-04-11 00:11:51 +0300

    1.0.0

--- WatchShop/App_Start/FilterConfig.cs
+++ WatchShop/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using WatchShop.Infrastructure;
 
 namespace WatchShop
 {
@@ -8,6 +9,7 @@ namespace WatchShop
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            DependencyResolver.SetResolver(new ShopDependencyResolver());
         }
     }
 }
---
+++ WatchShop/Infrastructure/ShopDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatchShop.Infrastructure
+{
+    public class ShopDependencyResolver : IDependencyResolver
+    {
+        IKernel kernel;
+        public ShopDependencyResolver()
+        {
+            kernel = new StandardKernel();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
